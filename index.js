73a535974663b832fecd0a28ee0a956e96e712f9
akash3410 <mdarifuzzaman982k@gmail.com>
2024-03-04 17:02:51 +0600

    Redux-Basic

---
+++ index.js
@@ -0,0 +1,45 @@
+const redux = require('redux');
+
+let initState = {
+  num: 5
+}
+
+const rootReducer = (state = initState, action) => {
+  if (action.type === "ADD_NUM") {
+    return {
+      ...state,
+      num: state.num + 1
+    }
+  }
+  if (action.type === "INC_NUM") {
+    return {
+      ...state,
+      num: action.value + state.num
+    }
+  }
+  if (action.type === "DEC_NUM") {
+    return {
+      ...state,
+      num: state.num - action.value
+    }
+  }
+  return state;
+}
+
+const store = redux.createStore(rootReducer);
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "ADD_NUM"
+})
+store.dispatch({
+  type: "INC_NUM",
+  value: 38
+})
+store.dispatch({
+  type: "DEC_NUM",
+  value: 24
+})
+
+console.log(store.getState());
